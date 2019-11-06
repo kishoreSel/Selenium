1e02d1aa470090acae6ba781d2295ae2c66299fc
vasudeva <vasu.584@gmail.com>
2019-11-06 06:18:04 +0530

    complete project

---
+++ Kishore/src/test/java/vasudeva/AppTest.java
@@ -0,0 +1,14 @@
+package vasudeva;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class AppTest 
+{
+	@Test
+	public void sample()
+	{
+		FirefoxDriver driver=new FirefoxDriver();
+		driver.get("Http://gmail.com");
+	}
+}
